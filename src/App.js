719df5000b1a1794c39be70d770e7a2f30f3566b
Hazel161 <hazelvuong@gmail.com>
2024-08-18 11:42:26 +0930

    minor fix

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ const App = () => {
                 <Route path="/showcase" element={<Showcase />} />
                 <Route path="/contact" element={<Contact />} />
                 <Route path="/hazelvuong" element={<AboutMe />} />
-                <Route path="/" element={<AboutMe />} />
+                {/*<Route path="/" element={<AboutMe />} />*/}
             </Routes>
                     </Content>
             </Layout>
